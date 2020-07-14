4aee5cfa2ec847c87b2eb97606dc35af89bafb23
Chris Norman <chris.norman2@googlemail.com>
2020-07-14 18:30:56 +0100

    Fixed decorator calls to take advantage of new default can_run.

--- example.py
+++ example.py
@@ -38,27 +38,27 @@ if __name__ == '__main__':
             except SynthizerError as e:
                 tts.speak(str(e))
 
-    @g.action('Quit', symbol=key.ESCAPE, can_run=g.normal)
+    @g.action('Quit', symbol=key.ESCAPE)
     def do_quit():
         """Quit the game."""
         g.window.close()
 
     @g.action(
         'Beep', symbol=key.B, interval=0.75,
-        can_run=lambda: g.can_beep and g.no_menu
+        can_run=lambda: g.can_beep and g.normal()
     )
     def do_beep():
         """Speak something."""
         sys.stdout.write('\a')
         sys.stdout.flush()
 
-    @g.action('Toggle beeping', symbol=key.P, can_run=g.normal)
+    @g.action('Toggle beeping', symbol=key.P)
     def toggle_beep():
         """Toggle beeping."""
         g.can_beep = not g.can_beep
         tts.speak(f'Beeping {"enabled" if g.can_beep else "disabled"}.')
 
-    @g.action('Menu', symbol=key.M, can_run=g.normal)
+    @g.action('Menu', symbol=key.M)
     def menu():
         """Select a file."""
         menu = FileMenu(
@@ -67,14 +67,13 @@ if __name__ == '__main__':
         g.push_menu(menu)
 
     @g.action(
-        'Show actions', symbol=key.SLASH, modifiers=key.MOD_SHIFT,
-        can_run=g.normal
+        'Show actions', symbol=key.SLASH, modifiers=key.MOD_SHIFT
     )
     def show_actions():
         """Show all game actions."""
         g.push_menu(ActionMenu(g))
 
-    @g.action('Set window title', symbol=key.T, can_run=g.normal)
+    @g.action('Set window title', symbol=key.T)
     def set_title():
         """Change the window title."""
         yield
