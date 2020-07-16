e1205b50a62acf69030d34d648d94ce8a9c5b731
Chris Norman <chris.norman2@googlemail.com>
2020-07-16 09:12:45 +0100

    Use a local file for sound example.

--- example.py
+++ example.py
@@ -23,7 +23,7 @@ class ExampleGame(Game):
             self.ctx, BufferGenerator(self.ctx)
         )
         self.menu_sound_player.generator.buffer = get_buffer(
-            self.ctx, 'file', r'..\terraform\sounds\menus\activate.wav'
+            self.ctx, 'file', 'move.wav'
         )
 
 
