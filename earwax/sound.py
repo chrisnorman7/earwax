aa6fa025e813c27f2e75d8760c8229510f641014
Chris Norman <chris.norman2@googlemail.com>
2020-07-20 20:41:55 +0100

    Changed get_buffer signature.

--- earwax/sound.py
+++ earwax/sound.py
@@ -13,7 +13,7 @@ NoneType = type(None)
 buffers = {}
 
 
-def get_buffer(context, protocol, path):
+def get_buffer(protocol, path):
     """Get a Buffer instance. Buffers are cached in the buffers dictionary, so
     if there is already a buffer with the given protocol and path, it will be
     returned. Otherwise, a new buffer will be created, and added to
