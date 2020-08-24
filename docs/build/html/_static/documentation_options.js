d6c32b4f36cdd86c5724525b2db41b3f5f445996
Chris Norman <chris.norman2@googlemail.com>
2020-08-24 11:00:28 +0100

    Cleaned up custom JavaScript.

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.0.6.dev16',
+    VERSION: '6.0.6.dev18',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -11,7 +11,7 @@ function addCopyButtonToCode(){
     // create a button that's configured for clipboard.js
     // point it to the text that's in this code block
     // add the button just after the text in the code block w/ jquery
-    var clipButton = '<button class="btn copybtn" data-clipboard-target="#' + currentId + '"><img src="https://clipboardjs.com/assets/images/clippy.svg" width="13" alt="Copy to clipboard"></button>';
+    let clipButton = '<button class="btn copybtn" data-clipboard-target="#' + currentId + '"><img src="https://clipboardjs.com/assets/images/clippy.svg" width="13" alt="Copy to clipboard"></button>';
     $(this).after(clipButton);
   });
 
@@ -22,5 +22,4 @@ function addCopyButtonToCode(){
 $(document).ready(function () {
   // Once the DOM is loaded for the page, attach clipboard buttons
   addCopyButtonToCode();
- });
- 
+});
