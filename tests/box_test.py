03fef39252c7a3b4a75ed3cb2e564436d8fffc49
Chris Norman <chris.norman2@googlemail.com>
2020-09-01 06:33:31 +0100

    Updated tests to reflect recent changes.

--- tests/box_test.py
+++ tests/box_test.py
@@ -10,7 +10,6 @@ def test_init() -> None:
     assert b.bottom_left == Point(0, 0)
     assert b.top_right == Point(3, 3)
     assert b.surface_sound is None
-    assert b.surface_generator is None
     assert b.context is None
     assert b.source is None
     c: Box = Box(b.bottom_left, b.top_right, parent=b)
