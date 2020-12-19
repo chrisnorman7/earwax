5e8434fb0928ab67bbc2c4394bb8c2b532a6c244
Chris Norman <chris.norman2@googlemail.com>
2020-12-19 21:28:11 +0000

    Updated the map demo.

--- map_demo.py
+++ map_demo.py
@@ -1,174 +1,105 @@
 """Map demo."""
 
 from pathlib import Path
-from typing import Generator, List
+from typing import List, Optional
 
-from pyglet.window import Window, key, mouse
+from pyglet.window import Window, key
 
-from earwax import (Ambiance, Box, BoxLevel, Door, Editor, FittedBox, Game,
-                    Point, Portal, box_row)
-from earwax.cmd.constants import sounds_directory, surfaces_directory
+from earwax import (Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point,
+                    play_and_destroy)
 
-wall_sounds = sounds_directory / 'walls'
+sounds_directory: Path = Path('sounds')
+footsteps_directory: Path = sounds_directory / 'footsteps'
 
-boxes: List[Box] = [
-    Box(
-        Point(0, 0, 0), Point(99, 2, 2), name='Main Corridor',
-        surface_sound=surfaces_directory / 'gridwork'
-    ),
-    Box(
-        Point(0, 3, 0), Point(0, 12, 2), wall=True, wall_sound=wall_sounds,
-        name='Western wall'
-    ),
-    Box(
-        Point(0, -1, 0), Point(100, -1, 2), wall=True, wall_sound=wall_sounds,
-        name='Southern wall'
-    ),
-    Box(
-        Point(100, 0, 0), Point(100, 2, 2), wall=True, wall_sound=wall_sounds,
-        name='Fill up the eastern wall'
-    )
-]
+walls: List[Box] = []
+doors: List[Box] = []
 
-door_sounds_directory: Path = sounds_directory / 'doors'
-index: int
-box: Box
-for index, box in enumerate(box_row(
-    Point(1, 4, 0), Point(19, 9, 2), 5, Point(2, 0, 0))
-):
-    box.surface_sound = surfaces_directory / 'concrete'
-    box.name = f'Office {index + 1}'
-    boxes.append(box)
-    door_coordinates: Point = box.start + Point(1, -1, 0)
-    door: Box = Box(
-        door_coordinates, door_coordinates, name='Office Entrance',
-        surface_sound=surfaces_directory / 'concrete', door=Door(
-            open=False, closed_sound=door_sounds_directory / 'closed.wav',
-            open_sound=door_sounds_directory / 'open.wav',
-            close_sound=door_sounds_directory / 'close.wav', close_after=(
-                3.0, 5.0
-            )
+
+class MyBox(Box):
+    """A box with a collition event attached."""
+
+    def on_collide(self, coordinates: Point) -> None:
+        """Play the right sound."""
+        play_and_destroy(
+            game.audio_context, sounds_directory / 'collide.wav',
+            position=coordinates.coordinates
+        )
+        return super().on_collide(coordinates)
+
+
+def finalise_office(office: MyBox):
+    """Add a wall and a door."""
+    door: Door = Door()
+    start: Point = office.start + Point(3, -1, 0)
+    end: Point = Point(start.x, start.y, office.end.z)
+    doors.append(
+        MyBox(
+            start, end, door=door, name=f'Door to {office.name}',
+            surface_sound=office.surface_sound
         )
     )
-    boxes.append(door)
-
-boxes.extend(
-    box_row(
-        Point(20, 4, 0), Point(1, 9, 2), 5, Point(20, 0, 0),
-        wall=True, wall_sound=wall_sounds
+    walls.append(
+        MyBox(
+            office.bounds.bottom_back_right + Point(1, 0, 0),
+            office.end + Point(1, 0, 0),
+            type=BoxTypes.solid
+        )
     )
+
+
+offices: List[MyBox] = MyBox.create_row(
+    Point(0, 4, 0), Point(7, 10, 2), 5, Point(2, 0, 0),
+    get_name=lambda i: f'Office {i + 1}',
+    surface_sound=footsteps_directory / 'office', on_create=finalise_office
 )
 
-boxes.extend(
-    box_row(
-        Point(1, 3, 0), Point(100, 1, 2), 2, Point(0, 10, 0),
-        wall=True, wall_sound=wall_sounds
+corridor: MyBox = MyBox(
+    offices[0].bounds.bottom_back_left - Point(0, 4, 0),
+    offices[-1].bounds.top_back_right - Point(0, 1, 0),
+    name='Corridor', surface_sound=footsteps_directory / 'corridor'
+)
+wall: MyBox = MyBox(
+    corridor.bounds.bottom_front_left, corridor.end, type=BoxTypes.solid,
+    parent=corridor
+)
+
+boxes: List[MyBox] = offices + walls + doors + [corridor]
+
+game: Game = Game(name='Map Demo')
+
+box = MyBox.create_fitted(
+    boxes, name='Main Box', type=BoxTypes.solid,
+    pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1)
+)
+
+level: BoxLevel = BoxLevel(game, box, coordinates=corridor.start)
+
+level.action('Show coordinates', symbol=key.C)(level.show_coordinates())
+level.action('Show facing direction', symbol=key.F)(level.show_facing())
+level.action('Walk forwards', symbol=key.W, interval=0.5)(level.move())
+level.action(
+    'Walk backwards', symbol=key.S, interval=1.0
+)(level.move(distance=-0.5))
+level.action('Turn right', symbol=key.D)(level.turn(45))
+level.action('Turn left', symbol=key.A)(level.turn(-45))
+level.action('Activate nearby objects', symbol=key.RETURN)(level.activate())
+
+
+@level.action('Describe current box', symbol=key.X)
+def describe_current_box() -> None:
+    """Describe the current box."""
+    box: Optional[Box] = level.box.get_containing_box(
+        level.coordinates.floor()
     )
-)
-
-main_portal_point: Point = Point(99, 1, 0)
-main_portal_box: Box = Box(
-    main_portal_point, main_portal_point, name=boxes[0].name,
-    surface_sound=surfaces_directory / 'concrete'
-)
-boxes.append(main_portal_box)
-
-back_office: Box = Box(
-    Point(100, 100, 100), Point(125, 125, 102), name='Back Office'
-)
-back_portal_box: Box = Box(
-    back_office.start, back_office.start, name=back_office.name
-)
-
-boxes.extend(
-    [
-        back_office,
-        back_portal_box
-    ]
-)
-main_box: FittedBox = FittedBox(boxes, name='Error')
-
-game: Game = Game()
-window: Window = Window(caption='Map Demo')
-main_level: BoxLevel = BoxLevel(game, main_box)
-
-main_portal_box.portal = Portal(
-    main_level, back_office.start,
-    enter_sound=door_sounds_directory / 'open.wav'
-)
-back_portal_box.portal = Portal(
-    main_level, main_portal_point,
-    enter_sound=door_sounds_directory / 'close.wav'
-)
-
-main_level.ambiances.append(
-    Ambiance(
-        'file', str(sounds_directory / 'exit.wav'),
-        coordinates=Point(41.5, 3, 2)
-    )
-)
-
-main_level.action(
-    'Walk forwards', symbol=key.W, mouse_button=mouse.RIGHT, interval=0.4
-)(main_level.move())
-
-main_level.action('Turn left 45 degrees', symbol=key.A)(main_level.turn(-45))
-main_level.action('Turn right 45 degrees', symbol=key.D)(main_level.turn(45))
-main_level.action('About turn', symbol=key.S)(main_level.turn(180))
-
-main_level.action('Show facing', symbol=key.F)(main_level.show_facing())
-main_level.action(
-    'Show coordinates', symbol=key.C
-)(main_level.show_coordinates())
-
-main_level.action('Activate', symbol=key.RETURN)(main_level.activate())
-
-
-@main_level.action('Quit', symbol=key.ESCAPE)
-def do_quit() -> None:
-    """Quit the game."""
-    window.dispatch_event('on_close')
-
-
-@main_level.action('Goto', symbol=key.G)
-def goto() -> Generator[None, None, None]:
-    """Jump to some coordinates."""
-    dest: Point = main_level.coordinates.floor()
-    y_editor: Editor = Editor(game, text='%d' % main_level.coordinates.y)
-
-    @y_editor.event('on_submit')
-    def y_inner(value: str) -> None:
-        """Set the y coordinate, and jump the player."""
-        try:
-            dest.y = int(value)
-            if dest == main_level.coordinates:
-                game.output('Coordinates unchanged.')
-                return None
-            main_level.set_coordinates(dest)
-            game.output('Moved.')
-        except ValueError:
-            game.output('Invalid coordinate.')
-        finally:
-            game.pop_level()
-
-    x_editor: Editor = Editor(game, text='%d' % main_level.coordinates.x)
-
-    @x_editor.event('on_submit')
-    def x_inner(value: str) -> Generator[None, None, None]:
-        """Set the x coordinate."""
-        try:
-            dest.x = int(value)
-            yield
-            game.replace_level(y_editor)
-            game.output('Y coordinate: %d' % main_level.coordinates.y)
-        except ValueError:
-            game.output('Invalid coordinate.')
-            game.pop_level()
-
-    yield
-    game.push_level(x_editor)
-    game.output('X coordinate: %d' % main_level.coordinates.x)
+    if box is None:
+        game.output('No box.')
+    else:
+        b: BoxBounds = box.bounds
+        game.output(
+            f'{box.name}: {b.width + 1} x {b.depth + 1} x {b.height + 1}.'
+        )
 
 
 if __name__ == '__main__':
-    game.run(window, initial_level=main_level)
+    window: Window = Window(caption='Map Demo')
+    game.run(window, initial_level=level)
