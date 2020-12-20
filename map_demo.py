7a3f30fece7e966b851efe98bf59edbd42d78125
Chris Norman <chris.norman2@googlemail.com>
2020-12-20 15:05:33 +0000

    Removed subclasses.

--- map_demo.py
+++ map_demo.py
@@ -3,14 +3,13 @@
 from pathlib import Path
 from typing import List, Optional
 
-from attr import Factory, attrs
 from pyglet.window import Window, key
-from synthizer import Context, GlobalFdnReverb
+from synthizer import GlobalFdnReverb
 
-from earwax import (Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point,
-                    play_and_destroy)
+from earwax import Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point
 
 sounds_directory: Path = Path('sounds')
+wall_sound: Path = sounds_directory / 'collide.wav'
 footsteps_directory: Path = sounds_directory / 'footsteps'
 
 walls: List[Box] = []
@@ -43,36 +42,6 @@ class OfficeReverb(GlobalFdnReverb):
         self.t60 = 0.699999988079071
 
 
-@attrs(auto_attribs=True)
-class MyBox(Box):
-    """A box with a collition event attached."""
-
-    surface_sound: Optional[Path] = None
-    wall_sound: Path = Factory(lambda: sounds_directory / 'collide.wav')
-
-
-class MyBoxLevel(BoxLevel):
-    """Override a couple of methods."""
-
-    def collide(self, box: Box, coordinates: Point) -> None:
-        """Play the appropriate sound."""
-        if self.game.audio_context is not None and box.wall_sound is not None:
-            play_and_destroy(
-                self.game.audio_context, box.wall_sound,
-                position=coordinates.coordinates, reverb=box.reverb
-            )
-        return super().collide(box, coordinates)
-
-    def on_move(self) -> None:
-        """Play a footstep sound."""
-        box: Optional[Box] = self.get_current_box()
-        if box is not None:
-            ctx: Optional[Context] = self.game.audio_context
-            if ctx is not None and box.surface_sound is not None:
-                play_and_destroy(ctx, box.surface_sound, reverb=box.reverb)
-        return super().on_move()
-
-
 game: Game = Game(name='Map Demo')
 
 
@@ -81,53 +50,54 @@ def before_run() -> None:
     """Create rooms and level."""
     office_reverb: OfficeReverb = OfficeReverb(game.audio_context)
 
-    def finalise_office(office: MyBox):
+    def finalise_office(office: Box):
         """Add walls and a door."""
         door: Door = Door()
         start: Point = office.start + Point(3, 0, 0)
         end: Point = Point(start.x, start.y, office.end.z)
         doors.append(
-            MyBox(
+            Box(
                 start, end, door=door, name=f'Door to {office.name}',
                 surface_sound=office.surface_sound, reverb=office_reverb,
-                parent=office
+                parent=office, wall_sound=wall_sound
             )
         )
         walls.extend(
             [
-                MyBox(
+                Box(
                     office.bounds.bottom_back_right + Point(1, 0, 0),
                     office.end + Point(1, 0, 0),
-                    type=BoxTypes.solid
-                ), MyBox(
+                    type=BoxTypes.solid, wall_sound=wall_sound
+                ), Box(
                     office.start, end - Point(1, 0, 0), parent=office,
-                    type=BoxTypes.solid
-                ), MyBox(
+                    type=BoxTypes.solid, wall_sound=wall_sound
+                ), Box(
                     start + Point(1, 0, 0), office.bounds.top_back_right,
-                    parent=office, type=BoxTypes.solid
+                    parent=office, type=BoxTypes.solid, wall_sound=wall_sound
                 )
             ]
         )
 
-    offices: List[MyBox] = MyBox.create_row(
+    offices: List[Box] = Box.create_row(
         Point(0, 4, 0), Point(7, 10, 2), 5, Point(2, 0, 0),
         get_name=lambda i: f'Office {i + 1}', on_create=finalise_office,
-        surface_sound=footsteps_directory / 'office', reverb=office_reverb
+        surface_sound=footsteps_directory / 'office', reverb=office_reverb,
+        wall_sound=wall_sound
     )
     corridor_reverb: GlobalFdnReverb = GlobalFdnReverb(game.audio_context)
     corridor_reverb.gain = 0.25
-    corridor: MyBox = MyBox(
+    corridor: Box = Box(
         offices[0].bounds.bottom_back_left - Point(0, 4, 0),
         offices[-1].bounds.top_back_right - Point(0, 1, 0),
         name='Corridor', surface_sound=footsteps_directory / 'corridor',
-        reverb=corridor_reverb
+        reverb=corridor_reverb, wall_sound=wall_sound
     )
-    boxes: List[MyBox] = offices + walls + [corridor]
-    box = MyBox.create_fitted(
+    boxes: List[Box] = offices + walls + [corridor]
+    box = Box.create_fitted(
         boxes, name='Main Box', type=BoxTypes.solid,
         pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1)
     )
-    level: MyBoxLevel = MyBoxLevel(game, box, coordinates=corridor.start)
+    level: BoxLevel = BoxLevel(game, box, coordinates=corridor.start)
     level.action('Show coordinates', symbol=key.C)(level.show_coordinates())
     level.action('Show facing direction', symbol=key.F)(level.show_facing())
     level.action('Walk forwards', symbol=key.W, interval=0.5)(level.move())
