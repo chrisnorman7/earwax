2a383b8d46264c0ecb989672ee78003ee390153a
Chris Norman <chris.norman2@googlemail.com>
2020-12-20 07:55:20 +0000

    Add reverb to the map.

--- map_demo.py
+++ map_demo.py
@@ -3,7 +3,9 @@
 from pathlib import Path
 from typing import List, Optional
 
+from attr import Factory, attrs
 from pyglet.window import Window, key
+from synthizer import GlobalFdnReverb
 
 from earwax import (Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point,
                     play_and_destroy)
@@ -15,16 +17,53 @@ walls: List[Box] = []
 doors: List[Box] = []
 
 
+class OfficeReverb(GlobalFdnReverb):
+    """A custom reverb.
+
+    Preset name: Office
+    """
+
+    name: str
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialise reverb."""
+        super().__init__(*args, **kwargs)
+        self.name = 'Untitled Reverb'
+        self.gain = 0.25
+        self.input_filter_cutoff = 2000.0
+        self.input_filter_enabled = True
+        self.late_reflections_delay = 0.009999999776482582
+        self.late_reflections_diffusion = 1.0
+        self.late_reflections_hf_rolloff = 0.5
+        self.late_reflections_lf_reference = 200.0
+        self.late_reflections_lf_rolloff = 1.0
+        self.late_reflections_modulation_depth = 0.009999999776482582
+        self.late_reflections_modulation_frequency = 0.5
+        self.mean_free_path = 0.019999999552965164
+        self.t60 = 0.699999988079071
+
+
+@attrs(auto_attribs=True)
 class MyBox(Box):
     """A box with a collition event attached."""
 
+    surface_sound: Optional[Path] = None
+    wall_sound: Path = Factory(lambda: sounds_directory / 'collide.wav')
+
     def on_collide(self, coordinates: Point) -> None:
         """Play the right sound."""
-        play_and_destroy(
-            game.audio_context, sounds_directory / 'collide.wav',
-            position=coordinates.coordinates
-        )
+        if game.audio_context is not None and self.wall_sound is not None:
+            play_and_destroy(
+                game.audio_context, self.wall_sound,
+                position=coordinates.coordinates, reverb=self.reverb
+            )
         return super().on_collide(coordinates)
+
+    def on_footstep(self) -> None:
+        """Play a footstep sound."""
+        if game.audio_context is not None and self.surface_sound is not None:
+            play_and_destroy(game.audio_context, self.surface_sound, reverb=self.reverb)
+        return super().on_footstep()
 
 
 def finalise_office(office: MyBox):
@@ -47,59 +86,64 @@ def finalise_office(office: MyBox):
     )
 
 
-offices: List[MyBox] = MyBox.create_row(
-    Point(0, 4, 0), Point(7, 10, 2), 5, Point(2, 0, 0),
-    get_name=lambda i: f'Office {i + 1}',
-    surface_sound=footsteps_directory / 'office', on_create=finalise_office
-)
-
-corridor: MyBox = MyBox(
-    offices[0].bounds.bottom_back_left - Point(0, 4, 0),
-    offices[-1].bounds.top_back_right - Point(0, 1, 0),
-    name='Corridor', surface_sound=footsteps_directory / 'corridor'
-)
-wall: MyBox = MyBox(
-    corridor.bounds.bottom_front_left, corridor.end, type=BoxTypes.solid,
-    parent=corridor
-)
-
-boxes: List[MyBox] = offices + walls + doors + [corridor]
-
 game: Game = Game(name='Map Demo')
 
-box = MyBox.create_fitted(
-    boxes, name='Main Box', type=BoxTypes.solid,
-    pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1)
-)
 
-level: BoxLevel = BoxLevel(game, box, coordinates=corridor.start)
-
-level.action('Show coordinates', symbol=key.C)(level.show_coordinates())
-level.action('Show facing direction', symbol=key.F)(level.show_facing())
-level.action('Walk forwards', symbol=key.W, interval=0.5)(level.move())
-level.action(
-    'Walk backwards', symbol=key.S, interval=1.0
-)(level.move(distance=-0.5))
-level.action('Turn right', symbol=key.D)(level.turn(45))
-level.action('Turn left', symbol=key.A)(level.turn(-45))
-level.action('Activate nearby objects', symbol=key.RETURN)(level.activate())
-
-
-@level.action('Describe current box', symbol=key.X)
-def describe_current_box() -> None:
-    """Describe the current box."""
-    box: Optional[Box] = level.box.get_containing_box(
-        level.coordinates.floor()
+@game.event
+def before_run() -> None:
+    """Create rooms and level."""
+    office_reverb: OfficeReverb = OfficeReverb(game.audio_context)
+    offices: List[MyBox] = MyBox.create_row(
+        Point(0, 4, 0), Point(7, 10, 2), 5, Point(2, 0, 0),
+        get_name=lambda i: f'Office {i + 1}', on_create=finalise_office,
+        surface_sound=footsteps_directory / 'office', reverb=office_reverb
     )
-    if box is None:
-        game.output('No box.')
-    else:
-        b: BoxBounds = box.bounds
-        game.output(
-            f'{box.name}: {b.width + 1} x {b.depth + 1} x {b.height + 1}.'
+    corridor_reverb: GlobalFdnReverb = GlobalFdnReverb(game.audio_context)
+    corridor_reverb.gain = 0.25
+    corridor: MyBox = MyBox(
+        offices[0].bounds.bottom_back_left - Point(0, 4, 0),
+        offices[-1].bounds.top_back_right - Point(0, 1, 0),
+        name='Corridor', surface_sound=footsteps_directory / 'corridor', reverb=corridor_reverb
+    )
+    MyBox(
+        corridor.bounds.bottom_front_left, corridor.end, type=BoxTypes.solid,
+        parent=corridor
+    )
+    boxes: List[MyBox] = offices + walls + doors + [corridor]
+    box = MyBox.create_fitted(
+        boxes, name='Main Box', type=BoxTypes.solid,
+        pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1)
+    )
+    level: BoxLevel = BoxLevel(game, box, coordinates=corridor.start)
+    level.action('Show coordinates', symbol=key.C)(level.show_coordinates())
+    level.action('Show facing direction', symbol=key.F)(level.show_facing())
+    level.action('Walk forwards', symbol=key.W, interval=0.5)(level.move())
+    level.action(
+        'Walk backwards', symbol=key.S, interval=1.0
+    )(level.move(distance=-0.5))
+    level.action('Turn right', symbol=key.D)(level.turn(45))
+    level.action('Turn left', symbol=key.A)(level.turn(-45))
+    level.action(
+        'Activate nearby objects', symbol=key.RETURN
+    )(level.activate())
+
+    @level.action('Describe current box', symbol=key.X)
+    def describe_current_box() -> None:
+        """Describe the current box."""
+        box: Optional[Box] = level.box.get_containing_box(
+            level.coordinates.floor()
         )
+        if box is None:
+            game.output('No box.')
+        else:
+            b: BoxBounds = box.bounds
+            game.output(
+                f'{box.name}: {b.width + 1} x {b.depth + 1} x {b.height + 1}.'
+            )
+
+    game.push_level(level)
 
 
 if __name__ == '__main__':
     window: Window = Window(caption='Map Demo')
-    game.run(window, initial_level=level)
+    game.run(window)
