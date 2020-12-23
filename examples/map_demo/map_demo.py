c498b9573d7139152683b494b01ddcd74556305b
Chris Norman <chris.norman2@googlemail.com>
2020-12-23 15:17:31 +0000

    Update map demo to work with the new reverb settings.

--- examples/map_demo/map_demo.py
+++ examples/map_demo/map_demo.py
@@ -4,9 +4,11 @@ from pathlib import Path
 from typing import List, Optional
 
 from pyglet.window import Window, key
-from synthizer import GlobalFdnReverb
+from synthizer import DirectSource, GlobalFdnReverb, Source3D
+from synthizer_fx.reverb import ReverbDict
 
-from earwax import Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point
+from earwax import (Box, BoxBounds, BoxLevel, BoxTypes, Door, Game, Point,
+                    SoundManager)
 
 sounds_directory: Path = Path('sounds')
 wall_sound: Path = sounds_directory / 'collide.wav'
@@ -15,32 +17,15 @@ footsteps_directory: Path = sounds_directory / 'footsteps'
 walls: List[Box] = []
 doors: List[Box] = []
 
+office_reverb: ReverbDict = {
+    'gain': 0.25,
+    't60': 0.9
+}
 
-class OfficeReverb(GlobalFdnReverb):
-    """A custom reverb.
-
-    Preset name: Office
-    """
-
-    name: str
-
-    def __init__(self, *args, **kwargs) -> None:
-        """Initialise reverb."""
-        super().__init__(*args, **kwargs)
-        self.name = 'Untitled Reverb'
-        self.gain = 0.25
-        self.input_filter_cutoff = 2000.0
-        self.input_filter_enabled = True
-        self.late_reflections_delay = 0.009999999776482582
-        self.late_reflections_diffusion = 1.0
-        self.late_reflections_hf_rolloff = 0.5
-        self.late_reflections_lf_reference = 200.0
-        self.late_reflections_lf_rolloff = 1.0
-        self.late_reflections_modulation_depth = 0.009999999776482582
-        self.late_reflections_modulation_frequency = 0.5
-        self.mean_free_path = 0.019999999552965164
-        self.t60 = 0.699999988079071
-
+corridor_reverb: ReverbDict = {
+    'gain': 0.25,
+    't60': 1.0
+}
 
 game: Game = Game(name='Map Demo')
 
@@ -48,7 +33,12 @@ game: Game = Game(name='Map Demo')
 @game.event
 def before_run() -> None:
     """Create rooms and level."""
-    office_reverb: OfficeReverb = OfficeReverb(game.audio_context)
+    player_sound_manager: SoundManager = SoundManager(
+        game.audio_context, DirectSource(game.audio_context)
+    )
+    external_sound_manager: SoundManager = SoundManager(
+        game.audio_context, Source3D(game.audio_context)
+    )
 
     def finalise_office(office: Box):
         """Add walls and a door."""
@@ -58,8 +48,9 @@ def before_run() -> None:
         doors.append(
             Box(
                 start, end, door=door, name=f'Door to {office.name}',
-                surface_sound=office.surface_sound, reverb=office_reverb,
-                parent=office, wall_sound=wall_sound
+                surface_sound=office.surface_sound,
+                reverb_settings=office_reverb, parent=office,
+                wall_sound=wall_sound
             )
         )
         walls.extend(
@@ -81,23 +72,28 @@ def before_run() -> None:
     offices: List[Box] = Box.create_row(
         Point(0, 4, 0), Point(7, 10, 2), 5, Point(2, 0, 0),
         get_name=lambda i: f'Office {i + 1}', on_create=finalise_office,
-        surface_sound=footsteps_directory / 'office', reverb=office_reverb,
-        wall_sound=wall_sound
+        surface_sound=footsteps_directory / 'office',
+        reverb_settings=office_reverb, wall_sound=wall_sound
     )
-    corridor_reverb: GlobalFdnReverb = GlobalFdnReverb(game.audio_context)
-    corridor_reverb.gain = 0.25
     corridor: Box = Box(
         offices[0].bounds.bottom_back_left - Point(0, 4, 0),
         offices[-1].bounds.top_back_right - Point(0, 1, 0),
         name='Corridor', surface_sound=footsteps_directory / 'corridor',
-        reverb=corridor_reverb, wall_sound=wall_sound
+        reverb_settings=corridor_reverb, wall_sound=wall_sound
     )
     boxes: List[Box] = offices + walls + [corridor]
     box = Box.create_fitted(
         boxes, name='Main Box', type=BoxTypes.solid,
-        pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1)
+        pad_start=Point(-1, -1, -1), pad_end=Point(1, 1, 1),
+        wall_sound=wall_sound
     )
-    level: BoxLevel = BoxLevel(game, box, coordinates=corridor.start)
+    reverb: GlobalFdnReverb = GlobalFdnReverb(game.audio_context)
+    level: BoxLevel = BoxLevel(
+        game, box, coordinates=corridor.start,
+        player_sound_manager=player_sound_manager,
+        external_sound_manager=external_sound_manager
+    )
+    level.connect_reverb(reverb)
     level.action('Show coordinates', symbol=key.C)(level.show_coordinates())
     level.action('Show facing direction', symbol=key.F)(level.show_facing())
     level.action('Walk forwards', symbol=key.W, interval=0.5)(level.move())
