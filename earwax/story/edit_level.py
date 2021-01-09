fa31cc8eb825462352f723504601cbc497c89c55
Chris Norman <chris.norman2@googlemail.com>
2021-01-09 19:45:45 +0000

    Made it possible to describe a room.

--- earwax/story/edit_level.py
+++ earwax/story/edit_level.py
@@ -1,10 +1,11 @@
 """Provides the EditLevel class."""
 
-from typing import Callable, List, Optional
+from typing import Callable, Generator, List, Optional
 
 from attr import attrs
 from shortuuid import uuid
 
+from ..editor import Editor
 from ..game import Game
 from ..menu import Menu
 from .play_level import PlayLevel
@@ -55,6 +56,10 @@ class EditLevel(PlayLevel):
         )
         self.action('Go to another room', symbol=key.G)(self.goto_room)
         self.action('Create room', symbol=key.R)(self.create_room)
+        self.action('Describe this room', symbol=key.D)(self.describe_room)
+        self.action(
+            'Clone description', symbol=key.D, modifiers=key.MOD_SHIFT
+        )(self.clone_description)
         return super().__attrs_post_init__()
 
     def save(self) -> None:
@@ -66,6 +71,37 @@ class EditLevel(PlayLevel):
         except Exception as e:
             self.game.output(str(e))
             raise
+
+    def describe_room(self) -> Generator[None, None, None]:
+        """Set the description for the current room."""
+        r: WorldRoom = self.state.room
+        e: Editor = Editor(self.game, text=r.description)
+
+        @e.event
+        def on_submit(text: str) -> None:
+            """Set the description."""
+            self.game.pop_level()
+            r.description = text
+            self.game.output('Description set.')
+
+        yield
+        self.game.push_level(e)
+        self.game.output('Enter a new description: %s' % r.description)
+
+    def clone_description(self) -> None:
+        """Set the description of this room from another room."""
+        current_room: WorldRoom = self.state.room
+
+        def inner(new_room: WorldRoom) -> None:
+            """Set the description of this room."""
+            current_room.description = f'#{new_room.id}'
+            self.game.output('Description set.')
+
+        push_rooms_menu(
+            self.game,
+            [r for r in self.world.rooms.values() if r is not current_room],
+            inner
+        )
 
     def goto_room(self) -> None:
         """Let the player choose a room to go to."""
