f432e7c1b3f1ccceb4a8d40838295586c794b71f
Chris Norman <chris.norman2@googlemail.com>
2021-03-04 14:31:01 +0000

    Make a proper tree structure.

--- earwax/conversation_level.py
+++ earwax/conversation_level.py
@@ -1,17 +1,16 @@
 """Provides the CallResponseLevel class, and various supporting classes."""
 
 import os.path
-from enum import Enum
 from pathlib import Path
-from typing import Dict, Generic, List, Optional, Tuple, Type, TypeVar, Union
+from typing import Dict, List, Optional, Tuple, TypeVar, Union
 
-from attr import Factory, attrib, attrs
+from attr import Factory, attrs
 from pyglet.window import key
 from shortuuid import uuid
 
 from .config import Config, ConfigValue
 from .editor import Editor
-from .hat_directions import DOWN, UP
+from .hat_directions import DOWN, LEFT, RIGHT, UP
 from .level import Level
 from .menus.menu import Menu
 from .mixins import DumpLoadMixin
@@ -21,18 +20,11 @@ T = TypeVar("T")
 WaitType = Optional[Union[float, Tuple[float, float]]]
 
 
-class CurrentPage(Enum):
-    """The current page."""
-
-    sections = 0
-    finishers = 1
-
-
 @attrs(auto_attribs=True)
 class ConversationBase(DumpLoadMixin):
     """A base for conversations and finishers."""
 
-    id: str
+    id: str = Factory(uuid)
     text: Optional[str] = None
     sound: Optional[str] = None
 
@@ -50,6 +42,7 @@ class ConversationSection(ConversationBase):
     """A part of a conversation."""
 
     before_wait: WaitType = None
+    after_wait: WaitType = None
     response_ids: List[str] = Factory(list)
     finisher_ids: List[str] = Factory(list)
 
@@ -60,19 +53,18 @@ class Finisher(ConversationBase):
 
 
 ItemType = Union[ConversationSection, Finisher]
-CallDict = Dict[str, ConversationSection]
-CallList = List[ConversationSection]
+ConversationSectionDict = Dict[str, ConversationSection]
 FinisherDict = Dict[str, Finisher]
-FinisherList = List[Finisher]
 
 
 @attrs(auto_attribs=True)
 class ConversationTree(DumpLoadMixin):
     """A structure for holding conversation sections and finishers."""
 
-    sections: CallDict = Factory(dict)
+    sections: ConversationSectionDict = Factory(dict)
     finishers: FinisherDict = Factory(dict)
     initial_section_id: Optional[str] = None
+    winning_section_ids: List[str] = Factory(list)
 
 
 class CallResponseSettings(Config):
@@ -87,13 +79,11 @@ class CallResponseSettings(Config):
 
 
 @attrs(auto_attribs=True)
-class EditorPage(Generic[T]):
-    """A page in the editor."""
+class ItemsStack:
+    """Store items."""
 
-    name: str
-    items: Dict[str, T]
-    bookmarks: Dict[int, str] = Factory(dict)
-    position: int = Factory(int)
+    items: List[ItemType]
+    position: int
 
 
 @attrs(auto_attribs=True)
@@ -102,44 +92,39 @@ class ConversationEditor(Level):
 
     tree: ConversationTree = Factory(ConversationTree)
     filename: Path = Factory(lambda: Path("tree.crt"))
-    page: CurrentPage = Factory(lambda: CurrentPage.sections)
-    pages: Dict[CurrentPage, EditorPage] = attrib()
-
-    @pages.default
-    def get_pages(
-        instance: "ConversationEditor",
-    ) -> Dict[CurrentPage, EditorPage]:
-        """Return a list of suitable pages."""
-        return {
-            CurrentPage.sections: EditorPage(
-                "Conversation Sections",
-                instance.tree.sections,
-            ),
-            CurrentPage.finishers: EditorPage(
-                "Finishers", instance.tree.finishers
-            ),
-        }
+    items: List[ItemType] = Factory(list)
+    stack: List[ItemsStack] = Factory(list)
+    at_home: bool = False
+    current_position: int = 0
 
     def __attrs_post_init__(self) -> None:
         """Add actions."""
+        self.home(silent=True)
         self.action("Previous item", symbol=key.UP, hat_direction=UP)(
             self.previous_item
+        )
+        self.action("Collapse item", symbol=key.LEFT, hat_direction=LEFT)(
+            self.collapse_item
         )
         self.action("Next item", symbol=key.DOWN, hat_direction=DOWN)(
             self.next_item
         )
-        self.action(
-            "Conversation sections page", symbol=key._1, joystick_button=2
-        )(self.sections_page)
-        self.action("Finishers page", symbol=key._2, joystick_button=1)(
-            self.finishers_page
+        self.action("Expand item", symbol=key.RIGHT, hat_direction=RIGHT)(
+            self.expand_item
         )
         self.action("Set item text", symbol=key.T)(self.set_text)
         self.action("Set item sound", symbol=key.S)(self.set_sound)
-        self.action("New item", symbol=key.N)(self.new)
+        self.action("New conversation section", symbol=key.N)(self.new_section)
+        self.action("New finisher", symbol=key.N, modifiers=key.MOD_SHIFT)(
+            self.new_finisher
+        )
         self.action("Pick valid responses", symbol=key.R, joystick_button=3)(
             self.response_menu
         )
+        self.action("Pick possible finishers", symbol=key.F)(
+            self.finisher_menu
+        )
+        self.action("Set initial section", symbol=key.I)(self.set_initial_id)
         self.action(
             "Save", symbol=key.S, modifiers=key.MOD_CTRL, joystick_button=0
         )(self.save)
@@ -152,34 +137,36 @@ class ConversationEditor(Level):
         return super().__attrs_post_init__()
 
     @property
-    def current_page(self) -> EditorPage:
-        """Return the current page."""
-        return self.pages[self.page]
-
-    @property
-    def current_position(self) -> int:
-        """Return the current position in the current page."""
-        return self.current_page.position
-
-    @current_position.setter
-    def current_position(self, pos: int) -> None:
-        """Set the current position."""
-        self.current_page.position = pos
-
-    @property
-    def items_dict(self) -> Union[CallDict, FinisherDict]:
-        """Return the dictionary that the current page refers to."""
-        return self.current_page.items
-
-    @property
-    def items(self) -> Union[CallList, FinisherList]:
-        """Return a list of items on the current page."""
-        return list(self.current_page.items.values())
-
-    @property
     def current_item(self) -> ItemType:
         """Get the currently focused entry."""
         return self.items[self.current_position]
+
+    def home(self, silent: bool = False) -> None:
+        """Populate the items list with all items.
+
+        :param silent: If ``True``, the selected item will not be output.
+        """
+        self.at_home = True
+        self.items.clear()
+        self.items.extend(self.tree.sections.values())
+        self.items.extend(self.tree.finishers.values())
+        self.sort_items()
+        self.current_position = 0
+        if not silent:
+            self.output_item()
+
+    def sort_items(self) -> None:
+        """Sort items by ID."""
+
+        def k(item: ItemType) -> str:
+            if (
+                isinstance(item, ConversationSection)
+                and item.id == self.tree.initial_section_id
+            ):
+                return ""
+            return item.id
+
+        self.items.sort(key=k)
 
     def output_item(self) -> None:
         """Output the current item."""
@@ -195,10 +182,10 @@ class ConversationEditor(Level):
         if not self.items:
             return self.game.output("There are no items to show.")
         self.current_position += direction
-        if self.current_position >= len(self.items):
+        if self.current_position < 0:
             self.current_position = 0
-        elif self.current_position < 0:
-            self.current_position += len(self.items)
+        elif self.current_position >= len(self.items):
+            self.current_position = len(self.items) - 1
         self.output_item()
 
     def previous_item(self) -> None:
@@ -209,18 +196,48 @@ class ConversationEditor(Level):
         """Move down in the items list."""
         self.switch_item(1)
 
-    def set_page(self, page: CurrentPage) -> None:
-        """Switch pages."""
-        self.page = page
-        self.game.output(self.current_page.name)
+    def collapse_item(self) -> None:
+        """Move up to the previous level of items."""
+        try:
+            stack: ItemsStack = self.stack.pop()
+            self.items = stack.items
+            self.current_position = stack.position
+        except IndexError:
+            self.home(silent=True)
+        self.output_item()
 
-    def sections_page(self) -> None:
-        """Go to the previous page."""
-        self.set_page(CurrentPage.sections)
-
-    def finishers_page(self) -> None:
-        """Go to the next page."""
-        self.set_page(CurrentPage.finishers)
+    def expand_item(self) -> None:
+        """Move into the next level of items."""
+        if not self.at_home:
+            self.stack.append(ItemsStack(self.items, self.current_position))
+        else:
+            self.at_home = False
+        item: ItemType = self.current_item
+        s: ConversationSection
+        f: Finisher
+        if isinstance(item, ConversationSection):
+            self.items = [
+                s
+                for s in self.tree.sections.values()
+                if s.id in item.response_ids
+            ]
+            self.items.extend(
+                f
+                for f in self.tree.finishers.values()
+                if f.id in item.finisher_ids
+            )
+        else:
+            self.items = [
+                s
+                for s in self.tree.sections.values()
+                if item.id in s.finisher_ids
+            ]
+        if self.items:
+            self.sort_items()
+            self.current_position = 0
+            self.output_item()
+        else:
+            self.game.output("There are no items to show.")
 
     def set_text(self) -> OptionalGenerator:
         """Set the text for the currently focused item."""
@@ -276,17 +293,21 @@ class ConversationEditor(Level):
         self.tree.save(self.filename)
         self.game.output("Saved.")
 
-    def new(self) -> None:
-        """Create a new entry."""
-        cls: Type
-        if self.page is CurrentPage.sections:
-            cls = ConversationSection
-        else:
-            cls = Finisher
-        item: ItemType = cls(uuid(), text=f"Untitled {cls.__name__}")
-        self.items_dict[item.id] = item  # type:ignore[assignment]
-        self.current_position = 0
-        self.previous_item()
+    def new_section(self) -> None:
+        """Create a new conversation section."""
+        s: ConversationSection = ConversationSection(text="Untitled Section")
+        self.tree.sections[s.id] = s
+        self.home(silent=True)
+        self.current_position = self.items.index(s)
+        self.output_item()
+
+    def new_finisher(self) -> None:
+        """Create a new finisher."""
+        f: Finisher = Finisher(text="Untitled Finisher")
+        self.tree.finishers[f.id] = f
+        self.home(silent=True)
+        self.current_position = self.items.index(key.S)
+        self.output_item()
 
     def response_menu(self) -> OptionalGenerator:
         """Show a response menu for the current item."""
@@ -341,3 +362,69 @@ class ConversationEditor(Level):
             )
         yield
         self.game.push_level(m)
+
+    def finisher_menu(self) -> OptionalGenerator:
+        """Show a menu of finishers for the current item."""
+        item: ItemType = self.current_item
+        if isinstance(item, Finisher):
+            return self.game.output(
+                "Finishers cannot contain other finishers."
+            )
+
+        def add_finisher(finisher: Finisher) -> ActionFunctionType:
+            def inner() -> None:
+                assert isinstance(item, ConversationSection)
+                item.finisher_ids.append(finisher.id)
+                self.game.cancel("Done.")
+
+            return inner
+
+        def remove_finisher(
+            finisher: Finisher,
+        ) -> ActionFunctionType:
+            def inner() -> None:
+                assert isinstance(item, ConversationSection)
+                if finisher.id in item.finisher_ids:
+                    item.finisher_ids.remove(finisher.id)
+                self.game.cancel("Done.")
+
+            return inner
+
+        m: Menu = Menu(self.game, "Finishers")
+        i: str
+        finisher: Finisher
+        for i in item.finisher_ids:
+            finisher = self.tree.finishers[i]
+            m.add_item(
+                remove_finisher(finisher),
+                title=f"Remove {finisher.text}",
+                select_sound_path=None
+                if finisher.sound is None
+                else Path(finisher.sound),
+            )
+        for finisher in self.tree.finishers.values():
+            if finisher.id in item.finisher_ids:
+                continue
+            m.add_item(
+                add_finisher(finisher),
+                title=f"Add {finisher.text}",
+                select_sound_path=None
+                if finisher.sound is None
+                else Path(
+                    finisher.sound,
+                ),
+            )
+        yield
+        self.game.push_level(m)
+
+    def set_initial_id(self) -> None:
+        """Set the initial conversation section."""
+        if not self.tree.sections:
+            return self.game.output(
+                "There are currently no conversation sections."
+            )
+        item: ItemType = self.current_item
+        if isinstance(item, Finisher):
+            return self.game.output("You must select a conversation section.")
+        self.tree.initial_section_id = item.id
+        self.game.output("Initial section set.")
